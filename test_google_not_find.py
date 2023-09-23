c96d1f562c81db4c5304c89f57e5d7a2df9e7ee3
elenaanisimova <elena.anisimova@lenvendo.ru>
2023-09-23 20:39:27 +0200

    task 3_3. edit2 test_google_not_find.py

--- test_google_not_find.py
+++ test_google_not_find.py
@@ -5,5 +5,5 @@ from selene import be, have
 
 def test_google_not_find():
     browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('asdlkfjasdf12345').press_enter()
-    browser.element('[id="botstuff"]').should(have.text('По запросу asdlkfjasdf12345 ничего не найдено'))
+    browser.element('[name="q"]').should(be.blank).type('asdlkfjasdf123452222').press_enter()
+    browser.element('[id="botstuff"]').should(have.text('По запросу asdlkfjasdf123452222 ничего не найдено'))
