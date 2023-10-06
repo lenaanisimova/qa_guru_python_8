c9942f81c2e103f886c690ade4d5c9e0704865d7
elenaanisimova <elena.anisimova@lenvendo.ru>
2023-10-06 18:51:01 +0200

    task 5_2

--- tests/test_qa_demo.py
+++ tests/test_qa_demo.py
@@ -1,4 +1,4 @@
-from selene import browser, have
+from selene import browser, have, be
 import os
 
 def test_full_form():
@@ -7,14 +7,14 @@ def test_full_form():
     browser.element('#lastName').type('Anisimova')
     browser.element('#userEmail').type('test@gmail.com')
     browser.element(('css selector', 'label[for="gender-radio-2"]')).click()
-    browser.element('#userNumber').type('79123456789')
+    browser.element('#userNumber').type('7912345678')
     #Дата рождения
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').type('October')
     browser.element('.react-datepicker__year-select').type('1990')
     browser.element('.react-datepicker__day--002').click()
 
-    browser.element('#subjectsInput').type('History')
+    browser.element('#subjectsInput').type('History').press_enter()
     browser.element(('css selector', 'label[for="hobbies-checkbox-2"]')).click()
 
     #browser.element("#uploadPicture").click()
@@ -26,13 +26,15 @@ def test_full_form():
     browser.element('#react-select-4-input').type('Noida').press_enter()
     browser.element('#submit').press_enter()
     #Проверка перед отправкой
+    #browser.element('.modal-content').should(be.visible)
     browser.element('.table').all('td:nth-of-type(2)').should(have.texts(
+    #browser.all("tbody tr td:last-child").should(have.exact_texts(
         'Elena Anisimova',
         'test@gmail.com',
         'Female',
-        '79123456789',
-        '02 October 1990',
-        'Reading',
+        '7912345678',
+        '02 October,1990',
+        'History',
         'Reading',
         'iris.jpeg',
         'Volgograd',
